5e255380e6bc64371f9faaf92604155ec3212d0e
John <antoniojohnjr12@gmail.com>
2026-01-10 19:25:52 -0300

    Add files via upload

---
+++ amit.js
@@ -0,0 +1,24 @@
+console.log("hello work");
+const nome = "john";
+console.log('hello'+ name );
+let x = 11;
+const obj = {
+  nome: "john",
+  idade: 11,
+    cidade:"Demerval Lobão",
+};
+function soma(a, b){
+    return a + b;
+}
+console.log(soma(2,3));
+console.log(11 + 20);
+console.loge("ola desenvouvedor da web");
+function soma(a, b){
+    return 5, 10;
+}
+console.log("meu nome é john e minha idade é 11 anos e eu moro em dermerval lobão");
+const name = {
+    nome: "johny",
+    idade: 40,
+    cidade: "demerval lobão",
+}
